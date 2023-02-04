b7fd2e8471c5cac9db438132dd1c5a1679076d62
brianKungu <bkungu07@gmail.com>
2023-02-04 17:09:04 +0300

    diaspora description

--- pages/diaspora.js
+++ pages/diaspora.js
@@ -6,7 +6,17 @@ export default function diaspora() {
     <>
       <Meta title="Diaspora" description="Diaspora Properties" />
       <div className="px-4 my-4">
-        Diaspora
+        <section>
+          <p className="tracking-wide text-neutralTextColor text-lg">
+            Investing back home has never been this easy! At{" "}
+            <span className="text-secondaryBg font-semibold">Allied Properties</span>{" "}
+            we are committed to serve and deliver value to our clientelle. The
+            good news is that we have expanded our networks to serve Kenyans in
+            the Diaspora by providing prime and affordable properties. Our
+            Mission is to help you secure your investments with ease and through
+            the most seamless processes. We Deliver!
+          </p>
+        </section>
         <section>
           <NewsEvents />
         </section>
